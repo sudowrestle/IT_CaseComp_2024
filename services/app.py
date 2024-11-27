58a2271141bfba51071e2040f27f35a682fac366
A. Elia <thats_anthony@hotmail.com>
2024-11-26 19:27:53 -0500

    Restructure file tree

--- services/app.py
+++ services/app.py
@@ -1,6 +1,6 @@
 import flask
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, template_folder="../templates", static_folder="../static")
 
 @app.route('/')
 def home():
@@ -9,7 +9,7 @@ def home():
 @app.route('/submit', methods=['POST'])
 def submit():
     item_name = flask.request.form.get('Item_name')
-    return f"Yo this working??? Pulling data for... {item_name}"
+    return f"This working??? Pulling data for... {item_name}"
 
 if __name__ == "__main__":
     app.run()
