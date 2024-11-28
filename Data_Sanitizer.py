779bad1987b8fbd1896d840b214dfc07a1e88479
sudowrestle <thats_anthony@hotmail.com>
2024-11-28 01:44:17 -0500

    Decent model?

--- Data_Sanitizer.py
+++ Data_Sanitizer.py
@@ -12,14 +12,16 @@ df['EntryDate'] = pd.to_datetime(df['EntryDate'], format='%m/%d/%Y')
 
 df = df.sort_values(by=['Material', 'EntryDate'])
 
-# df['DayOfWeek'] = df['EntryDate'].dt.dayofweek
-# df['Month'] = df['EntryDate'].dt.month
-# df['DayOfYear'] = df['EntryDate'].dt.dayofyear
-# df['WeekOfYear'] = df['EntryDate'].dt.isocalendar().week
-#df['IsWeekend'] = df['DayOfWeek'].apply(lambda x: 1 if x >= 5 else 0)
+df['DayOfWeek'] = df['EntryDate'].dt.dayofweek
+df['Month'] = df['EntryDate'].dt.month
+df['DayOfYear'] = df['EntryDate'].dt.dayofyear
+df['WeekOfYear'] = df['EntryDate'].dt.isocalendar().week
+df['IsWeekend'] = df['DayOfWeek'].apply(lambda x: 1 if x >= 5 else 0)
 #df['DailyChange'] = df.groupby('Material')['Quantity'].diff()
 #rolling_window = 3
 #df['AvgIncreaseOrDecrease'] = df.groupby('Material')['DailyChange'].transform(lambda x: x.rolling(rolling_window, min_periods=1).mean())
+
+df = df.drop(columns='PurchaseOrder')
 
 df.to_csv('Sanitized/BAR1Sanitized.csv', index=False)
 
--- tabular_Test.py
+++ tabular_Test.py
@@ -12,28 +12,43 @@ print(df.dtypes)
 print(df.info())
 print(df.isnull().sum())
 
+date = pd.to_datetime("2024-05-28")
+
+day_of_week = date.dayofweek  # Monday=0, Sunday=6
+month = date.month
+day_of_year = date.dayofyear
+week_of_year = date.isocalendar().week
+is_weekend = 1 if day_of_week >= 5 else 0
+
 prediction_df = pd.DataFrame(
-    columns=['Material','Plant','UnitofMeasure','DebitCredit'],
+    columns=['Material', 'Plant', 'UnitofMeasure', 'DebitCredit', 'DayOfWeek', 'Month', 'DayOfYear', 'WeekOfYear', 'IsWeekend'],
     data={
     'Material': ['1010003'],
     'Plant': ['BAR1'],
     'UnitofMeasure': ['KG'],
-    'EntryDate': ['2023-09-09'],
     'DebitCredit': ['S'],
+    'DayOfWeek': [day_of_week],
+    'Month': [month],
+    'DayOfYear': [day_of_year],
+    'WeekOfYear': [week_of_year],
+    'IsWeekend': [is_weekend]
 })
 
-columns_to_keep = ['Material', 'Plant', 'UnitofMeasure', 'DebitCredit', 'Quantity']
+columns_to_keep = ['Material', 'Plant', 'UnitofMeasure', 'DebitCredit', 'Quantity', 'DayOfWeek', 'Month', 'DayOfYear', 'WeekOfYear', 'IsWeekend']
 df = df[columns_to_keep]
 
 dls = TabularDataLoaders.from_df(df, path=rel_path, y_names="Quantity", 
-    cat_names = ['Material', 'Plant','UnitofMeasure', 'DebitCredit'],
+    cat_names = ['Material', 'Plant', 'UnitofMeasure', 'DebitCredit', 'DayOfWeek', 'Month', 'DayOfYear', 'WeekOfYear', 'IsWeekend'],
     procs = [Categorify, FillMissing, Normalize])
 
-learn = tabular_learner(dls, metrics=[rmse,mae],loss_func=MSELossFlat())
-learn.fit_one_cycle(2)
+learn = tabular_learner(dls, metrics=accuracy)
+learn.fit_one_cycle(5)
+
+test_dl = learn.dls.test_dl(prediction_df)
+learn.get_preds(dl = test_dl)
 
 learn.show_results(max_n=15)
-row, clas, probs = learn.get_preds(prediction_df.iloc[0])
+row, clas, probs = learn.predict(prediction_df.iloc[0])
 print(f"Prediction quantity is: {clas.item()}")
 
 
