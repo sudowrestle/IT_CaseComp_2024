afbd5391a67ab13286c8ac9f9f6f127957c2c7ec
sudowrestle <thats_anthony@hotmail.com>
2024-11-28 00:41:56 -0500

    Wonk model

--- Data_Sanitizer.py
+++ Data_Sanitizer.py
@@ -2,22 +2,24 @@ import pandas as pd
 
 df = pd.read_csv('./Dummy Data/BAR1History.csv')
 
+df['Material'] =  "'" + df['Material'].astype(str) + "'"
+
 df['Plant'] = 'BAR1'
 
-df['Quantity'] = df['Quantity'].str.replace(',', '').astype(float)
+df['Quantity'] = df['Quantity'].str.replace(',', '').replace('"','').astype(float)
 
 df['EntryDate'] = pd.to_datetime(df['EntryDate'], format='%m/%d/%Y')
 
 df = df.sort_values(by=['Material', 'EntryDate'])
 
-df['DayOfWeek'] = df['EntryDate'].dt.dayofweek
-df['Month'] = df['EntryDate'].dt.month
-df['DayOfYear'] = df['EntryDate'].dt.dayofyear
-df['WeekOfYear'] = df['EntryDate'].dt.isocalendar().week
-df['IsWeekend'] = df['DayOfWeek'].apply(lambda x: 1 if x >= 5 else 0)
-df['DailyChange'] = df.groupby('Material')['Quantity'].diff()
-rolling_window = 3
-df['AvgIncreaseOrDecrease'] = df.groupby('Material')['DailyChange'].transform(lambda x: x.rolling(rolling_window, min_periods=1).mean())
+# df['DayOfWeek'] = df['EntryDate'].dt.dayofweek
+# df['Month'] = df['EntryDate'].dt.month
+# df['DayOfYear'] = df['EntryDate'].dt.dayofyear
+# df['WeekOfYear'] = df['EntryDate'].dt.isocalendar().week
+#df['IsWeekend'] = df['DayOfWeek'].apply(lambda x: 1 if x >= 5 else 0)
+#df['DailyChange'] = df.groupby('Material')['Quantity'].diff()
+#rolling_window = 3
+#df['AvgIncreaseOrDecrease'] = df.groupby('Material')['DailyChange'].transform(lambda x: x.rolling(rolling_window, min_periods=1).mean())
 
 df.to_csv('Sanitized/BAR1Sanitized.csv', index=False)
 
--- tabular_Test.py
+++ tabular_Test.py
@@ -1,44 +1,40 @@
 import os
-import pandas
-import datetime
-from fastai.tabular.all import *
+import pandas as pd
 from fastai.text.all import *
 from fastai.collab import *
+from fastai.tabular.all import *
 
+rel_path = os.path.dirname(__file__) + '/Sanitized/BAR1Sanitized.csv'
 
-local_path = os.path.dirname(__file__)
+df = pd.read_csv(rel_path)
 
-df = pd.read_csv('Sanitized/BAR1Sanitized.csv', parse_dates=['EntryDate'])
+print(df.dtypes)
+print(df.info())
+print(df.isnull().sum())
 
-cont_names = ['Material', 'Quantity', 'DayOfWeek', 'Month', 'DayOfYear', 'WeekOfYear', 'DailyChange', 'AvgIncreaseOrDecrease']
-cat_names = ['Plant', 'PurchaseOrder', 'UnitofMeasure', 'EntryDate']
-y_names = 'Quantity'
-
-procs = [Categorify, FillMissing, Normalize]
-
-splits = RandomSplitter(valid_pct=0.2)(range_of(df))
-to = TabularPandas(df, procs, cat_names, cont_names, y_names, splits=splits)
-
-dls = to.dataloaders(bs=64)
-
-learn = tabular_learner(dls, metrics=rmse)
-
-learn.fit_one_cycle(5, 1e-2)
-
-tomorrow_sample = pd.DataFrame({
-    'Material': [1010003],
+prediction_df = pd.DataFrame(
+    columns=['Material','Plant','UnitofMeasure','DebitCredit'],
+    data={
+    'Material': ['1010003'],
     'Plant': ['BAR1'],
-    'EntryDate': [pd.to_datetime('2024-11-28')],
-    'DayOfWeek': [pd.to_datetime('2024-11-28').dayofweek],
-    'Month': [11],
-    'DayOfYear': [332],
-    'WeekOfYear': [48],
-    'IsWeekend': [0],  
-    'DailyChange': [0],
-    'AvgIncreaseOrDecrease': [0] 
+    'UnitofMeasure': ['KG'],
+    'EntryDate': ['2023-09-09'],
+    'DebitCredit': ['S'],
 })
 
-transformed_sample = to.transform(tomorrow_sample)
+columns_to_keep = ['Material', 'Plant', 'UnitofMeasure', 'DebitCredit', 'Quantity']
+df = df[columns_to_keep]
 
-prediction = learn.predict(transformed_sample.iloc[0])
-print(f"Predicted Quantity for tomorrow: {prediction[0]}")
+dls = TabularDataLoaders.from_df(df, path=rel_path, y_names="Quantity", 
+    cat_names = ['Material', 'Plant','UnitofMeasure', 'DebitCredit'],
+    procs = [Categorify, FillMissing, Normalize])
+
+learn = tabular_learner(dls, metrics=[rmse,mae],loss_func=MSELossFlat())
+learn.fit_one_cycle(2)
+
+learn.show_results(max_n=15)
+row, clas, probs = learn.get_preds(prediction_df.iloc[0])
+print(f"Prediction quantity is: {clas.item()}")
+
+
+
